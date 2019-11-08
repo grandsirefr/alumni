cc8e88d94ed178f396bb1ee95874f995cf46c098
Francois Grandsire <grandsiref@gmail.com>
2019-11-08 08:46:33 +0100

    test

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -9,7 +9,18 @@
 namespace App\Controller\Admin;
 
 
-class AdminController
+use App\Repository\DegreeRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminController extends AbstractController
 {
 
+    /**
+     * @Route('/admin',name="admin.index")
+     */
+    public function index(DegreeRepository $degreeRepo){
+        $degrees=$degreeRepo->findAll();
+        return $this->render('admin/index.html.twig',['degrees'=>$degrees]);
+    }
 }
